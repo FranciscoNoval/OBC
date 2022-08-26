e6012430f3e35c5173387c5682e7ad041467d5b1
Francisco Miguel Noval <108024246+FranciscoNoval@users.noreply.github.com>
2022-08-26 11:11:23 -0300

    Commit Tema 3

---
+++ Java/JavaOBC/src/com/practicaobc/estructurasDeControl/EstructurasControl.java
@@ -0,0 +1,15 @@
+package com.practicaobc.estructurasDeControl;
+
+public class EstructurasControl {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Francisco", "Miguel", "Laura", "Beatriz"};
+
+        for (String nombre : nombres) {
+
+            System.out.print(nombre + " ");
+        }
+
+    }
+}
